46100489f075318511dbbdd62c45f3decbc0adb0
pshdev1030 <pshdev1030@gmail.com>
2022-10-26 20:52:11 +0900

    feat: 제네릭을 이용하여 ref에 타입 강제하도록 수정, ref를 다룰 수 있도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Text } from "./components";
 
 const Emphasis = ({ children }: { children: React.ReactNode }) => {
@@ -5,10 +6,13 @@ const Emphasis = ({ children }: { children: React.ReactNode }) => {
 };
 
 function App() {
+  const ref = useRef<HTMLAnchorElement>(null);
+  const ref2 = useRef<HTMLHeadingElement>(null);
+  // ref를 다루는데에 ref를 실제 핸들링 할 수 없고 타입체크를 할 수 없다.
   return (
     <div className="App">
       <Text as={Emphasis}>This is a text node with no as</Text>
-      <Text as="h1" color="blue" style={{ backgroundColor: "black" }}>
+      <Text as="h1" ref={ref} color="blue" style={{ backgroundColor: "black" }}>
         hi
       </Text>
       <Text as="div">hi</Text>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -32,26 +32,39 @@ type TextProps = {
 // }[keyof JSX.IntrinsicElements] |
 // ComponentType<P>;
 
-export const Text = <C extends React.ElementType = "span">({
-  as,
-  color,
-  style,
-  children,
-  ...restProps
-}: PolymorphicComponentProps<C, TextProps>) => {
-  const Component = as || "span";
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
 
-  //styled component와 같은 style을 고유한 classname으로 만들어주는 라이브러리를 사용
+type Props<C extends React.ElementType, P> = PolymorphicComponentProps<C, P>;
 
-  //대신 인라인 스타일을 사용
+type PolymorphicComponentPropsWithRef<
+  C extends React.ElementType,
+  P
+> = PolymorphicComponentProps<C, P> & { ref: PolymorphicRef<C> };
 
-  const internalStyles = color ? { style: { ...style, color } } : {};
-  return (
-    <Component {...restProps} {...internalStyles}>
-      {children}
-    </Component>
-  );
-};
+type TextComponent = <C extends React.ElementType>(
+  props: PolymorphicComponentPropsWithRef<C, TextProps>
+) => React.ReactElement | null;
+
+export const Text = React.forwardRef(
+  <C extends React.ElementType = "span">(
+    { as, color, style, children, ...restProps }: Props<C, TextProps>,
+    ref?: PolymorphicRef<C>
+  ) => {
+    const Component = as || "span";
+
+    //styled component와 같은 style을 고유한 classname으로 만들어주는 라이브러리를 사용
+
+    //대신 인라인 스타일을 사용
+
+    const internalStyles = color ? { style: { ...style, color } } : {};
+    return (
+      <Component {...restProps} {...internalStyles} ref={ref}>
+        {children}
+      </Component>
+    );
+  }
+);
 
 // <Text as="">hello world</Text>
 // 문제점 1 any는 HTML element가 아닌 값일 수도 있다.
